514e69070aa63115f48af2deb36e0235b569d738
mr-wang1009 <1115378579@qq.com>
2022-07-20 11:43:23 +0800

    feat: add type

--- src/type.ts
+++ src/type.ts
@@ -1,14 +1,15 @@
+export type Type = 'success' | 'error' | 'warning' | 'info'
+
 export interface Options {
   showMessage?: boolean
+  type?: Type
   duration?: number
   message?: string
   offset?: number
   center?: boolean
   showData?: boolean
 }
-export interface Message extends Options {
-
-}
+export type Message = Options
 export interface RemoveChildOptions {
   duration?: number
 }
